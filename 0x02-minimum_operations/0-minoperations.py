7486aa424cf71e31d1027df8d27be0dd149300b6
Peace-lov <chidieberepeace42@gmail.com>
2024-10-21 10:34:31 +0000

    updated

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -6,15 +6,14 @@ def minOperations(n):
     """
     Calculating the fewest number of operations needed to result in exactly n H characters
     """
-    if n < 2:
+    if (n < 2):
         return 0
-
     op = 0
     div = 2
-    while div > 1:
-        while n % div == 0:
+    while div <= n:
+        if n % div == 0:
             op += div
             n = n / div
+            div -= 1
         div += 1
-
     return op
