1381e6c7de6f819f0ae1d1c9efc28357cf75e3b2
Peace-lov <chidieberepeace42@gmail.com>
2024-10-21 10:16:17 +0000

    operation

---
+++ 0x02-minimum_operations/0-minoperations.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""Module for 0-minoperations"""
+
+
+def minOperations(n):
+    """
+    Calculating the fewest number of operations needed to result in exactly n H characters
+    """
+    if n < 2:
+        return 0
+
+    op = 0
+    div = 2
+    while div > 1:
+        while n % div == 0:
+            op += div
+            n = n / div
+        div += 1
+
+    return op
